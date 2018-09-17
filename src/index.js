4515864c2b7485a6a1191011a63400f5ab4a2b71
Chikara Chan <hongtu.cht@alibaba-inc.com>
2018-09-17 19:07:35 +0800

    feat: webpack lifecycle event

--- src/index.js
+++ src/index.js
@@ -1,25 +1,68 @@
-const { readFileSync } = require('fs');
+const { readFile } = require('fs');
+const { promisify } = require('util');
 const { resolve } = require('path');
 const browserslist = require('browserslist');
-const template = require('lodash.template');
+const readFileAsync = promisify(readFile);
 
 class ObsoleteWebpackPlugin {
   apply(compiler) {
-    if (compiler.hooks) {
-      compiler.hooks.emit.tapPromise(
-        this.constructor.name,
-        async compilation => await this.handleEmit(compilation)
-      );
-      return;
-    }
-    compiler.plugin('emit', async (compilation, callback) => {
-      await this.handleEmit(compilation);
-      callback();
-    });
+    compiler.hooks.compilation.tap(
+      this.constructor.name,
+      compilation => this.compilation(compilation)
+    );
+    compiler.hooks.emit.tapPromise(
+      this.constructor.name,
+      compilation => this.emit(compilation)
+    );
   }
-  async handleEmit(compilation) {
+  compilation(compilation) {
+    // const newChunk = compilation.addChunk('newChunk');
+    // newChunk.preventIntegration = true;
+    // entrypoint.unshiftChunk(newChunk);
+    // newChunk.addGroup(entrypoint);
+    // entrypoint.setRuntimeChunk(newChunk);
+
+    compilation.hooks.htmlWebpackPluginAlterChunks.tap(
+      this.constructor.name,
+      (chunks) => this.alterChunks(compilation, chunks)
+    );
+
+    // compilation.hooks.htmlWebpackPluginAlterAssetTags.tap(
+    //   this.constructor.name,
+    //   (htmlPluginData) => this.alterAssetTags(compilation, htmlPluginData)
+    // );
+  }
+  alterChunks(compilation, chunks) {
+    for (const entrypoint of compilation.entrypoints.values()) {
+      const newChunk = compilation.addChunk('newChunk');
+
+      newChunk.addGroup(entrypoint);
+      entrypoint.unshiftChunk(newChunk);
+      newChunk.files.push(`${compilation.outputOptions.publicPath}browser-update.js`);
+      debugger;
+    }
+
+    return chunks;
+  }
+  alterAssetTags(compilation, htmlPluginData) {
+    // htmlPluginData.body = [
+    //   ...htmlPluginData.body,
+    //   { 
+    //     tagName: 'script',
+    //     closeTag: true,
+    //     attributes: { 
+    //       type: 'text/javascript', 
+    //       src: `${compilation.outputOptions.publicPath}browser-update.js`,
+    //       async: true
+    //     } 
+    //   }
+    // ];
+    return htmlPluginData;
+  }
+  async emit(compilation) {
+    const libraryPath = resolve(__dirname, 'browser/obsolete.js');
+    const fileContent = await readFileAsync(libraryPath, 'utf-8');
     const browsers = browserslist();
-    const fileContent = readFileSync(resolve(__dirname, 'browser/obsolete.js'), 'utf-8');
     const concatedFileContent = this.concatFileContent(fileContent, browsers);
 
     compilation.assets['browser-update.js'] = {
