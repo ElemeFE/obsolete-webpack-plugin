b62cb04c7791d2c0ae4bddc049a3d07a89ebfe24
Chikara Chan <hongtu.cht@alibaba-inc.com>
2018-11-18 18:37:05 +0800

    feat: add minifier version

--- packages/obsolete-web/build/rollup.config.js
+++ packages/obsolete-web/build/rollup.config.js
@@ -1,11 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
+const config = ['obsolete', 'obsolete.min'].map(name => ({
   input: 'src/obsolete.js',
   output: {
-    file: 'dist/obsolete.js',
+    file: `dist/${name}.js`,
     format: 'umd',
     name: 'Obsolete',
   },
@@ -16,5 +17,8 @@ export default {
       runtimeHelpers: true,
       exclude: '../../node_modules/**',
     }),
+    ...(name.includes('min') ? [uglify()] : []),
   ],
-};
+}));
+
+export default config;
